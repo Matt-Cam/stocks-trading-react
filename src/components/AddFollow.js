b85c4530700ef0134f8fbf499343ab20a720463b
Matt <matthcameron@gmail.com>
2019-07-27 14:00:12 -0400

    addfollow uses real stocklist

--- src/components/AddFollow.js
+++ src/components/AddFollow.js
@@ -3,34 +3,33 @@ import { connect } from 'react-redux';
 import { addToWatchlist } from '../redux/actions';
 
 const AddFollow = ({ stocks, addToWatchlist }) => {
-    const [selected, setSelected] = useState();
-    
-    return (
-        <React.Fragment>
-            <h2>Select a new stock to follow</h2>
-            <select value={selected} onChange={(ev) => setSelected(ev.target.value)}>
+  const [selected, setSelected] = useState();
 
-                {stocks.map(stock => (
-                    <option 
-                        value={stock.symbol} 
-                        key={stock.symbol}
-                    >
-                        {stock.name}
-                    </option>
-                ))}
-            </select>
-            
-            <button className="modal__btn" onClick={() => addToWatchlist(selected)}>
-                Add
-            </button>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <h2>Select a new stock to follow</h2>
+      <select value={selected} onChange={ev => setSelected(ev.target.value)}>
+        {stocks.map(stock => (
+          <option value={stock.symbol} key={stock.symbol}>
+            {stock.name}
+          </option>
+        ))}
+      </select>
+
+      <button className='modal__btn' onClick={() => addToWatchlist(selected)}>
+        Add
+      </button>
+    </React.Fragment>
+  );
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addToWatchlist: (symbol) => dispatch(addToWatchlist({symbol}))
-    }
-}
+  return {
+    addToWatchlist: symbol => dispatch(addToWatchlist({ symbol }))
+  };
+};
 
-export default connect(null, mapDispatchToProps)(AddFollow)
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddFollow);
